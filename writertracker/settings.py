f82fe9ba3e997d3755f4d3e3e6ad12935dfc4144
valerieniloinsigh31 <valerieniloinsigh25@gmail.com>
2023-03-18 01:07:28 +0000

    Update secret key

--- writertracker/settings.py
+++ writertracker/settings.py
@@ -25,7 +25,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-#*a-*$dstw2xsexd4-z8agt=@a2oux7fsf-si$bi-^k**j5=p^'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
